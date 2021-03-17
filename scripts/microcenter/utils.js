d8c604473554a03169e3e46d54a501d1e6b523fd
Yorisoft <yelsinsepulveda@gmail.com>
2021-03-17 05:27:38 -0400

    Fixed micro center bot.

--- scripts/microcenter/utils.js
+++ scripts/microcenter/utils.js
@@ -6,6 +6,9 @@ selectors.set('singin_selector_2', '#ctl00_ContentLeftSide_ucLogin_btnSignIn');
 selectors.set('inventory_count', '#pnlInventory > p > span');
 selectors.set('add_to_cart_selector', '#options-button > form > input.btn-add.grey.big.STBTN');
 selectors.set('chekout_bttn_selector_1', '#ContentPlaceHolder1_ucOrderSummary_lbCheckout');
+selectors.set('grand_total_selector', '#ucOrderSummary_pnlSummary > div > div:nth-child(2) > div');
+selectors.set('phone_number_input_selector', '#ucCheckOut_rlvBillingAddress_ctrl0_txtPhoneNumber');
+selectors.set('review_bttn_selector', '#ucCheckOut_btnContinue');
 selectors.set('chekout_bttn_selector_2', '#ucOrderSummary_lbSubmitOrder');
 selectors.set('cvv_bttn_selector', '#ucCheckOut_txtCCCsv');
 
